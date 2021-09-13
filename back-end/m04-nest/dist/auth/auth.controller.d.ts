66909c9848edfa7dfdfda4dba94a81a6e99acdce
Matheus Rodrigues <matheusrodriguesoliveira273@gmail.com>
2021-09-13 17:34:53 -0300

    refactor(auth). refactor structure page

--- back-end/m04-nest/dist/auth/auth.controller.d.ts
+++ back-end/m04-nest/dist/auth/auth.controller.d.ts
@@ -3,5 +3,5 @@ import { LoginRequestBody } from './LoginRequestBody';
 export declare class AuthController {
     private readonly service;
     constructor(service: AuthService);
-    login({ email, senha }: LoginRequestBody): Promise<any>;
+    login({ email, senha }: LoginRequestBody): Promise<import("./UsuarioToken").UsuarioToken>;
 }
--- back-end/m04-nest/dist/auth/auth.controller.js
+++ back-end/m04-nest/dist/auth/auth.controller.js
@@ -11,6 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AuthController = void 0;
 const common_1 = require("@nestjs/common");
@@ -29,7 +30,7 @@ __decorate([
     (0, common_1.HttpCode)(common_1.HttpStatus.OK),
     __param(0, (0, common_1.Body)()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", [LoginRequestBody_1.LoginRequestBody]),
+    __metadata("design:paramtypes", [typeof (_a = typeof LoginRequestBody_1.LoginRequestBody !== "undefined" && LoginRequestBody_1.LoginRequestBody) === "function" ? _a : Object]),
     __metadata("design:returntype", void 0)
 ], AuthController.prototype, "login", null);
 AuthController = __decorate([
